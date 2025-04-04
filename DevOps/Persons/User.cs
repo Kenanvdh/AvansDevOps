3209d6160b51136c8a648c7fb7815dd75d77263a
Kenanvdh <kenanvdh@ziggo.nl>
2025-04-04 16:34:45 +0200

    minor change

--- DevOps/Persons/User.cs
+++ DevOps/Persons/User.cs
@@ -3,7 +3,6 @@
     public abstract class User
     {
         public string Name { get; set; }
-        private string Role { get; set; }
 
         public void SendMessage(string name, string message)
         {
