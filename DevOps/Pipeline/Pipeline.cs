6abbb41321fbae88a0bbf447a592d88ee37378cf
Kenanvdh <kenanvdh@ziggo.nl>
2025-04-05 14:17:55 +0200

    format change

--- DevOps/Pipeline/Pipeline.cs
+++ DevOps/Pipeline/Pipeline.cs
@@ -5,11 +5,11 @@ namespace Pipeline
 {
     public class Pipeline : IPipeline
     {
-public void Start(Sprint sprint)
-{
-    Console.WriteLine("Starting pipeline tasks");
-    SetState(new DownloadingSourcesState());            
-}
+        public void Start(Sprint sprint)
+        {
+            Console.WriteLine("Starting pipeline tasks");
+            SetState(new DownloadingSourcesState());
+        }
 
         public void SetState(IPipelineState state)
         {
