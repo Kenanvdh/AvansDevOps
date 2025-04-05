7d4a0b9a51f4cbefb36d0842a9138385c46b0919
Kenanvdh <kenanvdh@ziggo.nl>
2025-04-05 14:54:35 +0200

    'item' is null on at least one execution path.

--- DevOps/Persons/Developer.cs
+++ DevOps/Persons/Developer.cs
@@ -5,22 +5,25 @@
         public void CompleteItem(string ItemName, Sprints.Sprint sprint)
         {
             var item = sprint.BacklogItems.FirstOrDefault(i => i.Title == ItemName);
-            if (item.State != BacklogItems.BacklogItemState.Tested)
+            if (item != null)
             {
-                Console.WriteLine("Item is not in tested state");
-                return;
-            }
+                if (item.State != BacklogItems.BacklogItemState.Tested)
+                {
+                    Console.WriteLine("Item is not in tested state");
+                    return;
+                }
 
-            if (item.IsCompleted)
-            {
-                item.ChangeState(BacklogItems.BacklogItemState.Done);
-                Console.WriteLine("Item is  completed");
-            }
-            else
-            {
-                item.ChangeState(BacklogItems.BacklogItemState.ReadyForTesting);
-                Console.WriteLine("Item is not completed");
-            }     
+                if (item.IsCompleted)
+                {
+                    item.ChangeState(BacklogItems.BacklogItemState.Done);
+                    Console.WriteLine("Item is  completed");
+                }
+                else
+                {
+                    item.ChangeState(BacklogItems.BacklogItemState.ReadyForTesting);
+                    Console.WriteLine("Item is not completed");
+                }
+            } 
         }
     }
 }
--- DevOps/Persons/Tester.cs
+++ DevOps/Persons/Tester.cs
@@ -5,22 +5,24 @@
         public void TestItem(string itemName, Sprints.Sprint sprint)
         {
             var item = sprint.BacklogItems.FirstOrDefault(i => i.Title == itemName);
+            if (item == null)
+            {
+                if (item.State != BacklogItems.BacklogItemState.ReadyForTesting)
+                {
+                    Console.WriteLine("Item is not in testing state");
+                    return;
+                }
 
-            if (item.State != BacklogItems.BacklogItemState.ReadyForTesting)
-            {
-                Console.WriteLine("Item is not in testing state");
-                return;
-            }
-
-            if (!item.IsCompleted)
-            {
-                item.ChangeState(BacklogItems.BacklogItemState.ToDo);
-                Console.WriteLine("Item is not completed");
-            }
-            else
-            {
-                item.ChangeState(BacklogItems.BacklogItemState.Tested);
-                Console.WriteLine("Item is in progress");
+                if (!item.IsCompleted)
+                {
+                    item.ChangeState(BacklogItems.BacklogItemState.ToDo);
+                    Console.WriteLine("Item is not completed");
+                }
+                else
+                {
+                    item.ChangeState(BacklogItems.BacklogItemState.Tested);
+                    Console.WriteLine("Item is in progress");
+                }
             }
         }
     }
